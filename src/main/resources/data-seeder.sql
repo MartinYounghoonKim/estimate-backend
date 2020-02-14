483c36560e39e56bf3893eff1e17bc82658a5d33
martinyounghoonkim <hoons0131@gmail.com>
2020-02-14 23:14:26 +0900

    Add seed data

---
+++ src/main/resources/data-seeder.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (name, rate) values ('제품 1', '대용량 1L');
+INSERT INTO product (name, rate) values ('제품 2', '대용량 3L');
+INSERT INTO product (name, rate) values ('제품 3', '대용량 4L');
+INSERT INTO product (name, rate) values ('제품 4', '대용량 10L');
+
